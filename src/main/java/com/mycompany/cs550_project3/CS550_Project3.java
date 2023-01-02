826dbafafa9639b98f8f614af8db0754eaee81de
Nikita Patil <nikitapatil@Nikitas-MacBook-Air.local>
2023-01-02 13:57:03 +0530

    project upload

---
+++ src/main/java/com/mycompany/cs550_project3/CS550_Project3.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.cs550_project3;
+
+/**
+ *
+ * @author nikitapatil
+ */
+public class CS550_Project3 {
+
+    public static void main(String[] args) {
+        DataBaseConnectionForm.main(args);
+    }
+}
